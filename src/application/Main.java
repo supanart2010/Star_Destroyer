691def59b2b54c356467dd71376560004dc7b780
PoraRee <ree_inter5@hotmail.com>
2020-12-13 14:57:59 +0700

    Gui(Title)

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,16 +5,15 @@ import javafx.stage.Stage;
 
 public class Main extends Application{
 
-	
+	@Override
+	public void start(Stage primaryStage) {
+		// TODO Auto-generated method stub
+		primaryStage.setScene(SceneManager.loadTitleScene());
+		primaryStage.setTitle("Star Destroyer");
+		primaryStage.show();
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		launch(args);
 	}
-
-	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
