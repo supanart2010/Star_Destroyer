9edc1db8216d1ec3aebc8d9d7c7b160aac630f88
Supanart Barnsongkit <supanart2010@hotmail.com>
2020-12-17 16:58:43 +0700

    fix

--- src/bullets/BombBullet.java
+++ src/bullets/BombBullet.java
@@ -6,7 +6,6 @@ import rocket.Rocket;
 public class BombBullet extends Bullet {
 	public static final int BOMB_DELAYTIME = 3000;
 	public static final int BOMB_DAMAGE = 50;
-	private int radius;
 
 	public BombBullet(Rocket rocket) {
 		super(rocket);
@@ -14,5 +13,9 @@ public class BombBullet extends Bullet {
 		this.setSize(10, 10);
 		this.setPosition(rocket.getPositionX() + 40, rocket.getPositionY());
 		setBulletDamage(BOMB_DAMAGE);
+		
+		
+		
+		
 	}
 }
